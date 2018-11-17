0f7cb72c5bfdeabc18cbd96e50d57dc4cfadb4ce
Jeff Lewis <jeffylew77@gmail.com>
2018-11-16 17:19:30 -0800

    [Dev] Adding React Navigation + Bottom Tab Navigator

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+// Imports: Dependencies
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
+// React Native: App
 export default class App extends React.Component {
   render() {
     return (
@@ -11,6 +14,26 @@ export default class App extends React.Component {
   }
 }
 
+// React Navigation: Bottom Tab Navigator
+export default createBottomTabNavigator({
+  Explore: {
+    screen: Explore,
+  },
+  Saved: {
+    screen: Saved,
+  },
+  Trips: {
+    screen: Trips,
+  },
+  Inbox: {
+    screen: Inbox,
+  },
+  Profile: {
+    screen: Profile,
+  },
+})
+
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
