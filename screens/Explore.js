aefbed000e3d3071da88b51b7ebff61b79074f2a
Jeff Lewis <jeffylew77@gmail.com>
2018-11-17 19:49:53 -0800

    [Dev] Adding Header + Text Input

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,13 +1,26 @@
 // Imports: Dependencies
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // React Native: Explore
 export default class Explore extends React.Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.text}>Explore</Text>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.headerContent}>
+              <Icon name="ios-search" size={20} style={styles.headerIcon} />
+              <TextInput
+                placeholder="Search California"
+                placeholderTextColor="grey"
+                style
+              />
+            </View>
+
+          </View>
+        </View>
       </SafeAreaView>
     );
   }
@@ -15,12 +28,32 @@ export default class Explore extends React.Component {
 
 // Styles
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center',
   },
-  text: {
-    fontSize: 18,
+  header: {
+    height: 80,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: '#dddddd',
+    justifyContent: 'center',
+  },
+  headerContent: {
+    flexDirection: 'row',
+    fontSize: 24,
+    padding: 10,
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+  },
+  headerIcon: {
+    marginRight: 15,
+  },
+  headerText: {
+    flex: 1,
+    fontWeight: '700',
+    backgroundColor: 'white',
   }
 });
