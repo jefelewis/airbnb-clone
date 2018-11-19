68c2da0a3f18a34a2c543eec261fd9e9dfa3fa65
Jeff Lewis <jeffylew77@gmail.com>
2018-11-19 11:44:21 -0800

    [Dev] Adding Category screen

---
+++ components/Category.js
@@ -0,0 +1,41 @@
+// Imports: Dependencies
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+// React Native: Category
+export default class Category extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.image}>
+                <Image source={this.props.imageUri}
+                    style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
+                />
+            </View>
+            <View style={styles.text}>
+                <Text>{this.props.name}</Text>
+            </View>
+        </View>
+    );
+  }
+}
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    height: 130,
+    width: 130,
+    marginLeft: 20,
+    borderWidth: 0.5,
+    borderColor: '#dddddd',
+  },
+  image: {
+    flex: 2,
+  },
+  text: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+});
