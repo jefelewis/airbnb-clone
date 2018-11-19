0d48480607f55d98cc4e7fb5dbaf21bf1572e5c9
Jeff Lewis <jeffylew77@gmail.com>
2018-11-19 11:44:35 -0800

    [Dev] Importing App.js

--- App.js
+++ App.js
@@ -89,8 +89,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   image: {
     height: 24,
