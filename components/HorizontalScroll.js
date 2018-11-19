3d0b66a6a4b76ef842949cdf25e8599de0924fc6
Jeff Lewis <jeffylew77@gmail.com>
2018-11-19 12:42:05 -0800

    [Dev] Adding category + fixing comment

--- components/HorizontalScroll.js
+++ components/HorizontalScroll.js
@@ -6,26 +6,15 @@ import Icon from 'react-native-vector-icons/Ionicons';
 // Imports: Components
 import Category from '../components/Category';
 
-// Component: HorizontalScroll
+// Component: Horizontal Scroll
 export default class HorizontalScroll extends React.Component {
-  constructor(props) {
-    super();
-
-    this.state = {
-
-    }
-  }
-  
-  componentWillMount() {
-
-  }
-
   render() {
     return (
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.horizontalScroll}>
         <Category imageUri={require('../assets/airbnb/home.jpg')} name="Home"/>
         <Category imageUri={require('../assets/airbnb/experiences.jpg')} name="Experiences" />
         <Category imageUri={require('../assets/airbnb/restaurant.jpg')} name="Restaurant" />
+        <Category imageUri={require('../assets/airbnb/island.jpg')} name="Island Getaways" />
       </ScrollView>
     );
   }
